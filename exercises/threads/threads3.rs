9d300481a58015719d55074b29fed8ab37bad560
221220000-Huang Kun <1480049710@qq.com>
2024-10-02 22:14:27 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 use std::time::Duration;
 
@@ -26,23 +24,26 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Queue>, tx: Arc<Mutex<mpsc::Sender<u32>>>) {
+    let q1 = Arc::clone(&q);
+    let q2 = Arc::clone(&q);
+    let tx1 = Arc::clone(&tx);
+    let tx2 = Arc::clone(&tx);
 
     thread::spawn(move || {
-        for val in &qc1.first_half {
+        for val in &q1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            let tx1 = tx1.lock().unwrap(); 
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
-        for val in &qc2.second_half {
+        for val in &q2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            let tx2 = tx2.lock().unwrap(); 
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -50,7 +51,8 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
+    let tx = Arc::new(Mutex::new(tx)); 
+    let queue = Arc::new(Queue::new()); 
     let queue_length = queue.length;
 
     send_tx(queue, tx);
@@ -62,5 +64,5 @@ fn main() {
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue_length);
 }
